1c7feba37f510a063532053e117cafda02ca2cbe
Rosie <42421590+AlkyoneAoide@users.noreply.github.com>
2022-02-11 11:11:18 -0700

    Create discordwal.plugin.js

---
+++ discordwal.plugin.js
@@ -0,0 +1,10 @@
+/**
+ * @name Discord-Auto-Pywal
+ * @version 0.0.0
+ * @description A helper plugin for the associated theme that
+ * will refresh the theme whenever pywal is used to refresh
+ * the system colorscheme.
+ * 
+ * @author Rosie Hart
+ * @website https://github.com/AlkyoneAoide/discord-pywal/
+ */
